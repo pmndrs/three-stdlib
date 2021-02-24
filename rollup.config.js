b4d45672303bbb4da393c2641be8a40a02c8b477
Paul Henschel <drcmda@gmail.com>
2021-02-24 14:53:59 +0100

    build system

---
+++ rollup.config.js
@@ -0,0 +1,42 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+import glslify from 'rollup-plugin-glslify'
+import multiInput from 'rollup-plugin-multi-input'
+
+const root = process.platform === 'win32' ? path.resolve('/') : '/'
+const external = (id) => !id.startsWith('.') && !id.startsWith(root)
+const extensions = ['.js', '.jsx', '.ts', '.tsx', '.json']
+
+const getBabelOptions = ({ useESModules }, targets) => ({
+  babelrc: false,
+  extensions,
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+  presets: [
+    ['@babel/preset-env', { loose: true, modules: false, targets }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    ['transform-react-remove-prop-types', { removeImport: true }],
+    ['@babel/transform-runtime', { regenerator: false, useESModules }],
+  ],
+})
+
+export default [
+  {
+    input: ['src/**/*.js'],
+    output: { dir: `dist`, format: 'esm' },
+    external,
+    plugins: [
+      multiInput(),
+      json(),
+      glslify(),
+      babel(getBabelOptions({ useESModules: true }, '>1%, not dead, not ie 11, not op_mini all')),
+      resolve({ extensions }),
+    ],
+  },
+]
