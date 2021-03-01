1e2bd7402ab7158be29096a24f9b2458940839ad
onirenaud <renaud.rohlinger@gmail.com>
2021-03-01 14:59:52 +0900

    remove export @webxr-input-profiles/motion-controllers

--- src/webxr/XRControllerModelFactory.js
+++ src/webxr/XRControllerModelFactory.js
@@ -2,11 +2,7 @@ import { Mesh, MeshBasicMaterial, Object3D, Quaternion, SphereGeometry } from 't
 
 import { GLTFLoader } from '../loaders/GLTFLoader'
 
-import {
-  Constants as MotionControllerConstants,
-  fetchProfile,
-  MotionController,
-} from '@webxr-input-profiles/motion-controllers'
+import { Constants as MotionControllerConstants, fetchProfile, MotionController } from '@webxr-input-profiles'
 
 const DEFAULT_PROFILES_PATH = 'https://cdn.jsdelivr.net/npm/@webxr-input-profiles/assets@1.0/dist/profiles'
 const DEFAULT_PROFILE = 'generic-trigger'
