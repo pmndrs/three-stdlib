5302d15c74c803de3336d7ebbcbb688804b219dd
— <drcmda@gmail.com>
2022-12-31 09:51:39 +0100

    fix: remove ctor side effects from timer

--- src/misc/Timer.ts
+++ src/misc/Timer.ts
@@ -12,43 +12,38 @@ class Timer {
   constructor() {
     this._previousTime = 0
     this._currentTime = 0
-
     this._delta = 0
     this._elapsed = 0
-
     this._timescale = 1
-
     this._useFixedDelta = false
-    this._fixedDelta = 16.67 // ms, corresponds to approx. 60 FPS
-
-    // use Page Visibility API to avoid large time delta values
-
+    this._fixedDelta = 16.67 // ms, corresponds to approx. 60 FPS    
     this._usePageVisibilityAPI = typeof document !== 'undefined' && document.hidden !== undefined
-
-    if (this._usePageVisibilityAPI === true) {
-      this._pageVisibilityHandler = handleVisibilityChange.bind(this)
-
-      document.addEventListener('visibilitychange', this._pageVisibilityHandler, false)
-    }
   }
 
-  disableFixedDelta(): this {
-    this._useFixedDelta = false
-
+  // https://github.com/mrdoob/three.js/issues/20575
+  // use Page Visibility API to avoid large time delta values
+  connect(): this {    
+    if (this._usePageVisibilityAPI) {
+      this._pageVisibilityHandler = handleVisibilityChange.bind(this)
+      document.addEventListener('visibilitychange', this._pageVisibilityHandler, false)
+    }
     return this
   }
 
   dispose(): this {
-    if (this._usePageVisibilityAPI === true && this._pageVisibilityHandler) {
+    if (this._usePageVisibilityAPI && this._pageVisibilityHandler) {
       document.removeEventListener('visibilitychange', this._pageVisibilityHandler)
     }
+    return this
+  }
 
+  disableFixedDelta(): this {
+    this._useFixedDelta = false
     return this
   }
 
   enableFixedDelta(): this {
     this._useFixedDelta = true
-
     return this
   }
 
@@ -70,19 +65,16 @@ class Timer {
 
   reset(): this {
     this._currentTime = this._now()
-
     return this
   }
 
   setFixedDelta(fixedDelta: number): this {
     this._fixedDelta = fixedDelta * 1000
-
     return this
   }
 
   setTimescale(timescale: number): this {
     this._timescale = timescale
-
     return this
   }
 
@@ -92,14 +84,10 @@ class Timer {
     } else {
       this._previousTime = this._currentTime
       this._currentTime = this._now()
-
       this._delta = this._currentTime - this._previousTime
     }
-
     this._delta *= this._timescale
-
     this._elapsed += this._delta // _elapsed is the accumulation of all previous deltas
-
     return this
   }
 
