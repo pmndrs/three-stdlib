baa84a412981222de1490918910756c28eb13671
— <drcmda@gmail.com>
2023-01-23 15:52:59 +0100

    fix: update pointer lock

--- src/controls/PointerLockControls.ts
+++ src/controls/PointerLockControls.ts
@@ -1,52 +1,55 @@
-import { Camera, Euler, EventDispatcher, Vector3 } from 'three'
+import { Euler, Camera, EventDispatcher, Vector3 } from 'three'
+
+const _euler = new Euler(0, 0, 0, 'YXZ')
+const _vector = new Vector3()
+const _changeEvent = { type: 'change' }
+const _lockEvent = { type: 'lock' }
+const _unlockEvent = { type: 'unlock' }
+const _PI_2 = Math.PI / 2
 
 class PointerLockControls extends EventDispatcher {
-  private camera: Camera
+  public camera: Camera
   public domElement?: HTMLElement
-
-  public isLocked = false
-
-  // Set to constrain the pitch of the camera
-  // Range is 0 to Math.PI radians
-  public minPolarAngle = 0 // radians
-  public maxPolarAngle = Math.PI // radians
-
-  private changeEvent = { type: 'change' }
-  private lockEvent = { type: 'lock' }
-  private unlockEvent = { type: 'unlock' }
-
-  private euler = new Euler(0, 0, 0, 'YXZ')
-  private PI_2 = Math.PI / 2
-  private vec = new Vector3()
+  public isLocked: boolean
+  public minPolarAngle: number
+  public maxPolarAngle: number
+  public pointerSpeed: number
 
   constructor(camera: Camera, domElement?: HTMLElement) {
     super()
-    this.domElement = domElement
+
     this.camera = camera
-    if (this.domElement) this.connect(this.domElement)
+    this.domElement = domElement
+    this.isLocked = false
+
+    // Set to constrain the pitch of the camera
+    // Range is 0 to Math.PI radians
+    this.minPolarAngle = 0 // radians
+    this.maxPolarAngle = Math.PI // radians
+
+    this.pointerSpeed = 1.0
+    if (domElement) this.connect(domElement)
   }
 
   private onMouseMove = (event: MouseEvent): void => {
     if (!this.domElement || this.isLocked === false) return
-
     const movementX = event.movementX || (event as any).mozMovementX || (event as any).webkitMovementX || 0
     const movementY = event.movementY || (event as any).mozMovementY || (event as any).webkitMovementY || 0
-
-    this.euler.setFromQuaternion(this.camera.quaternion)
-    this.euler.y -= movementX * 0.002
-    this.euler.x -= movementY * 0.002
-    this.euler.x = Math.max(this.PI_2 - this.maxPolarAngle, Math.min(this.PI_2 - this.minPolarAngle, this.euler.x))
-    this.camera.quaternion.setFromEuler(this.euler)
-    this.dispatchEvent(this.changeEvent)
+    _euler.setFromQuaternion(this.camera.quaternion)
+    _euler.y -= movementX * 0.002 * this.pointerSpeed
+    _euler.x -= movementY * 0.002 * this.pointerSpeed
+    _euler.x = Math.max(_PI_2 - this.maxPolarAngle, Math.min(_PI_2 - this.minPolarAngle, _euler.x))
+    this.camera.quaternion.setFromEuler(_euler)
+    this.dispatchEvent(_changeEvent)
   }
 
   private onPointerlockChange = (): void => {
     if (!this.domElement) return
     if (this.domElement.ownerDocument.pointerLockElement === this.domElement) {
-      this.dispatchEvent(this.lockEvent)
+      this.dispatchEvent(_lockEvent)
       this.isLocked = true
     } else {
-      this.dispatchEvent(this.unlockEvent)
+      this.dispatchEvent(_unlockEvent)
       this.isLocked = false
     }
   }
@@ -55,7 +58,7 @@ class PointerLockControls extends EventDispatcher {
     console.error('THREE.PointerLockControls: Unable to use Pointer Lock API')
   }
 
-  public connect = (domElement?: HTMLElement): void => {
+  public connect = (domElement: HTMLElement): void => {
     this.domElement = domElement || this.domElement
     if (!this.domElement) return
     this.domElement.ownerDocument.addEventListener('mousemove', this.onMouseMove)
@@ -74,34 +77,35 @@ class PointerLockControls extends EventDispatcher {
     this.disconnect()
   }
 
-  private getObject = (): Camera =>
+  public getObject = (): Camera => {
     // retaining this method for backward compatibility
-    this.camera
+    return this.camera
+  }
 
   private direction = new Vector3(0, 0, -1)
-  public getDirection = (v: Vector3): Vector3 => v.copy(this.direction).applyQuaternion(this.camera.quaternion)
+  public getDirection = (v: Vector3): Vector3 => {
+    return v.copy(this.direction).applyQuaternion(this.camera.quaternion)
+  }
 
   public moveForward = (distance: number): void => {
     // move forward parallel to the xz-plane
-    // assumes this.camera.up is y-up
-    this.vec.setFromMatrixColumn(this.camera.matrix, 0)
-    this.vec.crossVectors(this.camera.up, this.vec)
-    this.camera.position.addScaledVector(this.vec, distance)
+    // assumes camera.up is y-up
+    _vector.setFromMatrixColumn(this.camera.matrix, 0)
+    _vector.crossVectors(this.camera.up, _vector)
+    this.camera.position.addScaledVector(_vector, distance)
   }
 
   public moveRight = (distance: number): void => {
-    this.vec.setFromMatrixColumn(this.camera.matrix, 0)
-    this.camera.position.addScaledVector(this.vec, distance)
+    _vector.setFromMatrixColumn(this.camera.matrix, 0)
+    this.camera.position.addScaledVector(_vector, distance)
   }
 
   public lock = (): void => {
-    if (!this.domElement) return
-    this.domElement.requestPointerLock()
+    if (this.domElement) this.domElement.requestPointerLock()
   }
 
   public unlock = (): void => {
-    if (!this.domElement) return
-    this.domElement.ownerDocument.exitPointerLock()
+    if (this.domElement) this.domElement.ownerDocument.exitPointerLock()
   }
 }
 
