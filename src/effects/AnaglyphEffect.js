d31e8bb3d15eb1ee5c7b8d6020d87b79c0d3d8b4
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-05-26 10:32:14 -0500

    fix: tree-shake effects

--- src/effects/AnaglyphEffect.js
+++ src/effects/AnaglyphEffect.js
@@ -12,154 +12,136 @@ import {
   WebGLRenderTarget,
 } from 'three'
 
-const AnaglyphEffect = function (renderer, width, height) {
-  // Dubois matrices from https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.7.6968&rep=rep1&type=pdf#page=4
+class AnaglyphEffect {
+  constructor(renderer, width = 512, height = 512) {
+    // Dubois matrices from https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.7.6968&rep=rep1&type=pdf#page=4
 
-  this.colorMatrixLeft = new Matrix3().fromArray([
-    0.4561,
-    -0.0400822,
-    -0.0152161,
-    0.500484,
-    -0.0378246,
-    -0.0205971,
-    0.176381,
-    -0.0157589,
-    -0.00546856,
-  ])
+    this.colorMatrixLeft = new Matrix3().fromArray([
+      0.4561,
+      -0.0400822,
+      -0.0152161,
+      0.500484,
+      -0.0378246,
+      -0.0205971,
+      0.176381,
+      -0.0157589,
+      -0.00546856,
+    ])
 
-  this.colorMatrixRight = new Matrix3().fromArray([
-    -0.0434706,
-    0.378476,
-    -0.0721527,
-    -0.0879388,
-    0.73364,
-    -0.112961,
-    -0.00155529,
-    -0.0184503,
-    1.2264,
-  ])
+    this.colorMatrixRight = new Matrix3().fromArray([
+      -0.0434706,
+      0.378476,
+      -0.0721527,
+      -0.0879388,
+      0.73364,
+      -0.112961,
+      -0.00155529,
+      -0.0184503,
+      1.2264,
+    ])
 
-  const _camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    const _camera = new OrthographicCamera(-1, 1, 1, -1, 0, 1)
 
-  const _scene = new Scene()
+    const _scene = new Scene()
 
-  const _stereo = new StereoCamera()
+    const _stereo = new StereoCamera()
 
-  const _params = {
-    minFilter: LinearFilter,
-    magFilter: NearestFilter,
-    format: RGBAFormat,
-  }
+    const _params = { minFilter: LinearFilter, magFilter: NearestFilter, format: RGBAFormat }
 
-  if (width === undefined) width = 512
-  if (height === undefined) height = 512
+    const _renderTargetL = new WebGLRenderTarget(width, height, _params)
+    const _renderTargetR = new WebGLRenderTarget(width, height, _params)
 
-  const _renderTargetL = new WebGLRenderTarget(width, height, _params)
-  const _renderTargetR = new WebGLRenderTarget(width, height, _params)
+    const _material = new ShaderMaterial({
+      uniforms: {
+        mapLeft: { value: _renderTargetL.texture },
+        mapRight: { value: _renderTargetR.texture },
 
-  const _material = new ShaderMaterial({
-    uniforms: {
-      mapLeft: { value: _renderTargetL.texture },
-      mapRight: { value: _renderTargetR.texture },
+        colorMatrixLeft: { value: this.colorMatrixLeft },
+        colorMatrixRight: { value: this.colorMatrixRight },
+      },
 
-      colorMatrixLeft: { value: this.colorMatrixLeft },
-      colorMatrixRight: { value: this.colorMatrixRight },
-    },
+      vertexShader: [
+        'varying vec2 vUv;',
 
-    vertexShader: [
-      'varying vec2 vUv;',
+        'void main() {',
 
-      'void main() {',
+        '	vUv = vec2( uv.x, uv.y );',
+        '	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
 
-      '	vUv = vec2( uv.x, uv.y );',
-      '	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+        '}',
+      ].join('\n'),
 
-      '}',
-    ].join('\n'),
+      fragmentShader: [
+        'uniform sampler2D mapLeft;',
+        'uniform sampler2D mapRight;',
+        'varying vec2 vUv;',
 
-    fragmentShader: [
-      'uniform sampler2D mapLeft;',
-      'uniform sampler2D mapRight;',
-      'varying vec2 vUv;',
+        'uniform mat3 colorMatrixLeft;',
+        'uniform mat3 colorMatrixRight;',
 
-      'uniform mat3 colorMatrixLeft;',
-      'uniform mat3 colorMatrixRight;',
+        'void main() {',
 
-      // These functions implement sRGB linearization and gamma correction
+        '	vec2 uv = vUv;',
 
-      'float lin( float c ) {',
-      '	return c <= 0.04045 ? c * 0.0773993808 :',
-      '			pow( c * 0.9478672986 + 0.0521327014, 2.4 );',
-      '}',
+        '	vec4 colorL = texture2D( mapLeft, uv );',
+        '	vec4 colorR = texture2D( mapRight, uv );',
 
-      'vec4 lin( vec4 c ) {',
-      '	return vec4( lin( c.r ), lin( c.g ), lin( c.b ), c.a );',
-      '}',
+        '	vec3 color = clamp(',
+        '			colorMatrixLeft * colorL.rgb +',
+        '			colorMatrixRight * colorR.rgb, 0., 1. );',
 
-      'float dev( float c ) {',
-      '	return c <= 0.0031308 ? c * 12.92',
-      '			: pow( c, 0.41666 ) * 1.055 - 0.055;',
-      '}',
+        '	gl_FragColor = vec4(',
+        '			color.r, color.g, color.b,',
+        '			max( colorL.a, colorR.a ) );',
 
-      'void main() {',
+        '	#include <tonemapping_fragment>',
+        '	#include <encodings_fragment>',
 
-      '	vec2 uv = vUv;',
+        '}',
+      ].join('\n'),
+    })
 
-      '	vec4 colorL = lin( texture2D( mapLeft, uv ) );',
-      '	vec4 colorR = lin( texture2D( mapRight, uv ) );',
+    const _mesh = new Mesh(new PlaneGeometry(2, 2), _material)
+    _scene.add(_mesh)
 
-      '	vec3 color = clamp(',
-      '			colorMatrixLeft * colorL.rgb +',
-      '			colorMatrixRight * colorR.rgb, 0., 1. );',
+    this.setSize = function (width, height) {
+      renderer.setSize(width, height)
 
-      '	gl_FragColor = vec4(',
-      '			dev( color.r ), dev( color.g ), dev( color.b ),',
-      '			max( colorL.a, colorR.a ) );',
+      const pixelRatio = renderer.getPixelRatio()
 
-      '}',
-    ].join('\n'),
-  })
+      _renderTargetL.setSize(width * pixelRatio, height * pixelRatio)
+      _renderTargetR.setSize(width * pixelRatio, height * pixelRatio)
+    }
 
-  const _mesh = new Mesh(new PlaneGeometry(2, 2), _material)
-  _scene.add(_mesh)
+    this.render = function (scene, camera) {
+      const currentRenderTarget = renderer.getRenderTarget()
 
-  this.setSize = (width, height) => {
-    renderer.setSize(width, height)
+      if (scene.matrixWorldAutoUpdate === true) scene.updateMatrixWorld()
 
-    const pixelRatio = renderer.getPixelRatio()
+      if (camera.parent === null && camera.matrixWorldAutoUpdate === true) camera.updateMatrixWorld()
 
-    _renderTargetL.setSize(width * pixelRatio, height * pixelRatio)
-    _renderTargetR.setSize(width * pixelRatio, height * pixelRatio)
-  }
+      _stereo.update(camera)
 
-  this.render = (scene, camera) => {
-    const currentRenderTarget = renderer.getRenderTarget()
+      renderer.setRenderTarget(_renderTargetL)
+      renderer.clear()
+      renderer.render(scene, _stereo.cameraL)
 
-    scene.updateMatrixWorld()
+      renderer.setRenderTarget(_renderTargetR)
+      renderer.clear()
+      renderer.render(scene, _stereo.cameraR)
 
-    if (camera.parent === null) camera.updateMatrixWorld()
+      renderer.setRenderTarget(null)
+      renderer.render(_scene, _camera)
 
-    _stereo.update(camera)
+      renderer.setRenderTarget(currentRenderTarget)
+    }
 
-    renderer.setRenderTarget(_renderTargetL)
-    renderer.clear()
-    renderer.render(scene, _stereo.cameraL)
-
-    renderer.setRenderTarget(_renderTargetR)
-    renderer.clear()
-    renderer.render(scene, _stereo.cameraR)
-
-    renderer.setRenderTarget(null)
-    renderer.render(_scene, _camera)
-
-    renderer.setRenderTarget(currentRenderTarget)
-  }
-
-  this.dispose = () => {
-    if (_renderTargetL) _renderTargetL.dispose()
-    if (_renderTargetR) _renderTargetR.dispose()
-    if (_mesh) _mesh.geometry.dispose()
-    if (_material) _material.dispose()
+    this.dispose = function () {
+      _renderTargetL.dispose()
+      _renderTargetR.dispose()
+      _mesh.geometry.dispose()
+      _mesh.material.dispose()
+    }
   }
 }
 
