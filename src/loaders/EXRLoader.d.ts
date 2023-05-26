51f82374319abb04bd5a5b36a9b2d8d134694622
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-05-26 18:28:56 -0500

    fix: EXRLoader is parse only

--- src/loaders/EXRLoader.d.ts
+++ src/loaders/EXRLoader.d.ts
@@ -13,15 +13,6 @@ export class EXRLoader extends DataTextureLoader {
   constructor(manager?: LoadingManager)
   type: TextureDataType
 
-  // @ts-ignore
-  load(
-    url: string,
-    onLoad: (geometry: EXR) => void,
-    onProgress?: (event: ProgressEvent) => void,
-    onError?: (event: ErrorEvent) => void,
-  ): void
-  // @ts-ignore
-  loadAsync(url: string, onProgress?: (event: ProgressEvent) => void): Promise<EXR>
   parse(buffer: ArrayBuffer): EXR
   setDataType(type: TextureDataType): this
 }
