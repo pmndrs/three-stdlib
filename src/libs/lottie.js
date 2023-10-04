d2741457d6f391583dc01544497847984a8464e0
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-10-04 08:21:10 -0500

    fix(lottie): harden SSR check

--- src/libs/lottie.js
+++ src/libs/lottie.js
@@ -1,5 +1,5 @@
 const lottie = (() => {
-  if (typeof navigator === 'undefined') return {}
+  if (typeof navigator === 'undefined' || typeof document === 'undefined') return {}
 
   const svgNS = 'http://www.w3.org/2000/svg'
 
