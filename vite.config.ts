69f0da84a036a38220a4e5030d6db819cf4dddb4
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-10-17 04:33:42 -0500

    experiment: preserve modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import * as path from 'node:path'
-import * as babel from '@babel/core'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
@@ -14,33 +13,9 @@ export default defineConfig({
     },
     rollupOptions: {
       external: (id: string) => !id.startsWith('.') && !path.isAbsolute(id),
-    },
-  },
-  plugins: [
-    {
-      name: 'vite-tree-shake',
-      renderChunk: {
-        order: 'post',
-        async handler(code) {
-          function annotate(path: babel.NodePath): void {
-            if (!path.getFunctionParent()) {
-              path.addComment('leading', '@__PURE__')
-            }
-          }
-
-          return babel.transform(code, {
-            sourceMaps: true,
-            plugins: [
-              {
-                visitor: {
-                  CallExpression: annotate,
-                  NewExpression: annotate,
-                },
-              },
-            ],
-          }) as any
-        },
+      output: {
+        preserveModules: true,
       },
     },
-  ],
+  },
 })
